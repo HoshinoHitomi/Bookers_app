5d0aef64ef0fd64dba6778b3f50b3d7a7bdd1225
HoshinoHitomi <hiikochan711@gmail.com>
2021-05-24 17:14:15 +0000

    [Update]投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,22 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
   end
 
   def show
   end
-
-  def new
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path #仮置き
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
